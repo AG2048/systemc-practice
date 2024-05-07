0e5d0f8c2e21b5a4182383e5428ba45778f27021
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-07 10:47:15 -0400

    moved fir to 2 files

---
+++ fir.hpp
@@ -0,0 +1,16 @@
+#include <systemc.h>
+
+SC_MODULE(fir){
+    sc_in<bool>         clk;
+    sc_in<bool>         rst;
+    sc_in<sc_int<16>>   inp;
+    sc_out<sc_int<16>>  outp;
+
+    void fir_main(); // leaving the behaviour codes to other files
+
+    SC_CTOR(fir){
+        SC_CTHREAD(fir_main, clk.pos()); // SC_CTHREAD runs fir_main when the clk sensitive edge occurs
+        reset_signal_is(rst, true); // Reset is asserted high, false will mean reset asserted low. This resets SC_CTHREAD
+
+    }
+}
