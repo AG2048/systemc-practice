a2f0b06cff4a6bfb2a774518c537b8ec2a004edb
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-07 12:52:25 -0400

    build files

---
+++ build/CMakeFiles/design.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/design.dir/fir.cpp.o"
+  "CMakeFiles/design.dir/fir.cpp.o.d"
+  "CMakeFiles/design.dir/main.cpp.o"
+  "CMakeFiles/design.dir/main.cpp.o.d"
+  "CMakeFiles/design.dir/tb.cpp.o"
+  "CMakeFiles/design.dir/tb.cpp.o.d"
+  "libdesign.a"
+  "libdesign.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/design.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
