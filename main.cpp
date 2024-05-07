731c321e2043b920b4e1c3091b022aeedfdb2a31
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-07 11:34:55 -0400

    add constructor

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <systemc.h>
+#include <fir.hpp>
+#include <tb.h>
+
+SC_MODULE(SYSTEM){
+    // Module declarations
+    tb      *tb0; // module_name    variable_name_pointer
+    fir     *fir0;
+
+    // Defining signals
+    sc_signal<sc_int<16>>   inp_sig;
+    sc_signal<sc_int<16>>   outp_sig;
+    sc_signal<bool>         rst_sig;
+    
+    // Defining a clock signal (I'm assuming it's a special class)
+    sc_clock clk_sig;
+
+    // Constructor
+    SC_CTOR(SYSTEM) 
+        : clk_sig("clk_sig", 10, SC_NS){ // construct clock signal -- naming it "clk_sig", 10 ns period, in NS unit
+        // Module instance signal connections
+        tb0 = new tb("tb0"); // defining tb0 as a new tb called "tb0"
+        tb0->clk(clk_sig); // tb0 is a pointer, we find its clk element and assign it clk_sig
+        tb0->rst(rst_sig);
+        tb0->inp(inp_sig);
+        tb0->outp(outp_sig);
+
+        fir0 = new fir("fir0"); // defining fir0 as a new fir called fir0
+        fir0->clk(clk_sig);
+        fir0->rst(rst_sig);
+        fir0->inp(inp_sig);
+        fir0->outp(outp_sig);
+    }
+
+    ~SYSTEM()
+        // Destructor
+}
