ba673016b67151ba87256dfc536615c8b587c8ef
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-07 12:08:53 -0400

    finished sink

--- tb.cpp
+++ tb.cpp
@@ -24,29 +24,12 @@ void tb::source(){
     }
 }
 
-// FIR Main thread (fir:: scopes fir_main under fir module)
-void fir::fir_main(void){
-    // This code continuously runs your multi-clock-cycle code, unless reset signal is received then it breaks out the while loop
-
-    // RESET behaviour (local var or outputs)
-    outp.write(0); // reset output
-
-    wait(); // automatically waits 1 cycle
-
-    while(true){
-        // Read inputs
-        for(int i = 4; i > 0; i--){
-            taps[i] = taps[i-1]; // shift data
-        }
-        taps[0] = input.read(); // load in new data
-        // Algo
-        sc_int<16> val;
-        for(int i = 0; i < 5; i++){
-            val += coef[i] * taps[i];
-        }
-        // Write
-        outp.write(val);
-        // Wait for clock boundary
+void tb::sink(){
+    sc_int<16> indata;
+    for(int i = 0; i < 64; i++){
+        indata = outp.read();
         wait();
+        cout << i << " \t: " << indata.to_int() << endl;
     }
+    sc_stop();
 }
