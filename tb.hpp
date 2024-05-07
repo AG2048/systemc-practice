028470abdca1c25f36f3805d2756ff2985d7490e
AG2048 <45613281+AG2048@users.noreply.github.com>
2024-05-07 12:46:40 -0400

    further debug, got it running

--- tb.hpp
+++ tb.hpp
@@ -4,7 +4,7 @@ SC_MODULE(tb){
     sc_in<bool>         clk;
     sc_out<bool>        rst;
     sc_out<sc_int<16>>  inp;
-    sc_out<sc_int<16>>  outp;
+    sc_in<sc_int<16>>  outp;
 
     void source(); // leaving the behaviour codes to other files
     void sink();
